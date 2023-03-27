a19be10adf6acb44ca9052fc73ad200a5f16dbc2
Gustavo Borges <gustavobprog@gmail.com>
2023-03-27 19:16:10 -0300

    Aula 1

---
+++ src/App.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String s;
+        int rating;
+        int i;
+        double ratingFloat;
+        // Fazer uma conexão HTTP e buscar os top 250 filmes
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI endereco = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String body = response.body();
+
+
+        // Pegar só os dados que interessam (titulo, poster, classificação)
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+
+        // Exibir e manupular os dados
+        for (Map<String,String> filme: listaDeFilmes) {
+            System.out.println("Título: " + filme.get("title"));
+            System.out.println("Poster: " + filme.get("image"));
+            s = filme.get("imDbRating");
+            ratingFloat = Double.parseDouble(s);
+            rating = (int) Math.floor(ratingFloat);
+            System.out.println("Classificação: " + s);
+            for(i=0; i<rating ; i++){
+                System.out.printf("⭐");
+            }
+
+            System.out.println();
+            System.out.println();
+        }
+    }
+}
